489cceec55d82deb27962215c17930054209cea1
Cristian T <Cristian.gorjeanul@gmail.com>
2020-03-19 10:54:41 +0200

    Small changes

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
     var form = document.querySelector('form');
     var inputs = document.querySelector('form').elements;
     var buttonDelete = document.querySelector('#button-delete');
     var deleteTableRow = document.querySelector('table tbody');
 
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
         var teamA = inputs[0].value;
         var scoreA = inputs[1].value;
         var teamB = inputs[2].value;
         var scoreB = inputs[3].value;
-        if(teamA !== '' && parseInt(scoreA, 10) >= 0 && teamB !== '' && parseInt(scoreB, 10) >= 0 && teamA !== teamB){
+        if (teamA !== '' && parseInt(scoreA, 10) >= 0 && teamB !== '' && parseInt(scoreB, 10) >= 0 && teamA !== teamB) {
             var newRow = document.createElement('tr');
             var teamATd = document.createElement('td');
             var teamBTd = document.createElement('td');
             var score = document.createElement('td');
-            teamATd.innerText = teamA;
-            teamBTd.innerText = teamB;
+            teamATd.innerText = teamA + "   ";
+            teamBTd.innerText = teamB + "   ";
             score.innerText = scoreA + " - " + scoreB;
             newRow.appendChild(teamATd);
             newRow.appendChild(teamBTd);
@@ -29,9 +29,9 @@ document.addEventListener('DOMContentLoaded', function(){
             inputs[3].value = '';
         }
     });
-    buttonDelete.addEventListener('click', function(){
+    buttonDelete.addEventListener('click', function () {
         var lastElem = deleteTableRow.lastElementChild;
         lastElem.remove();
     });
-    
+
 });
